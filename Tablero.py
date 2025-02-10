b2edb77d2c005a7f7a6faaeb078c8049f5a873cc
pedromeixu <pedromeixu44@gmail.com>
2025-02-10 14:28:44 +0100

    funcion agregar barcos

--- Tablero.py
+++ Tablero.py
@@ -1,3 +1,13 @@
 class Tablero:
     def __init__(self, tamaño=8):
         self.tamaño = tamaño
+        self.barcos = []
+        self.disparos = []
+
+    def agregar_barcos(self, barco):
+        for barcos in self.barcos:
+            for coordenadas in barcos:
+                if coordenadas in barcos:
+                    raise ValueError("Ya hay un barco en esa posición")
+                else:
+                    self.barcos.append(barco)
